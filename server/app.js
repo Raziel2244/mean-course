73d255694250690b1414d056270ecc59cb1dd98d
Elliot <ethomas283@gmail.com>
2019-01-30 01:22:52 +0000

    Finished Image Upload Chapter

--- server/app.js
+++ server/app.js
@@ -1,27 +1,38 @@
+const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const postsRoutes = require('./routes/posts');
+const postsRoutes = require("./routes/posts");
 
 const app = express();
 
-mongoose.connect("mongodb+srv://node:mHwTylQ4VrMG3rIG@cluster0-jffrs.gcp.mongodb.net/test?retryWrites=true", { useNewUrlParser: true })
+mongoose
+  .connect(
+    "mongodb+srv://node:mHwTylQ4VrMG3rIG@cluster0-jffrs.gcp.mongodb.net/test?retryWrites=true",
+    { useNewUrlParser: true }
+  )
   .then(() => {
-    console.log('Connected to database!')
-  }).catch(() => {
-    console.log('Failed to connect to database!')
+    console.log("Connected to database!");
+  })
+  .catch(() => {
+    console.log("Failed to connect to database!");
   });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: "false" }));
+app.use("/images", express.static(path.join("server/images")));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Headers",
-  "Origin, X-Requested-With, Content-Type, Accept");
-  res.setHeader("Access-Control-Allow-Methods",
-  "GET, POST, PATCH, PUT, DELETE, OPTIONS")
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+  );
   next();
 });
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import {
   MatButtonModule,
@@ -28,7 +28,7 @@ import { PostListComponent } from './posts/post-list/post-list.component';
   imports: [
     BrowserAnimationsModule,
     BrowserModule,
-    FormsModule,
+    ReactiveFormsModule,
     HttpClientModule,
     MatButtonModule,
     MatCardModule,

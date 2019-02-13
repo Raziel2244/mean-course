3ba4c90b3ac33acc4192c5485028380b82ba685b
Elliot <raziel2244@pm.me>
2019-02-13 23:08:27 +0000

    118 - Using User ID in front end

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -61,7 +61,8 @@ router.put("/:id", checkAuth, multer({
     _id: req.body.id,
     title: req.body.title,
     content: req.body.content,
-    imagePath: imagePath
+    imagePath: imagePath,
+    creator: req.userData.userId
   });
   Post.updateOne({
     _id: req.params.id,
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -50,6 +50,7 @@ export class PostsService {
       title: string;
       content: string;
       imagePath: string;
+      creator: string;
     }>("http://localhost:3000/api/posts/" + id);
   }
 
@@ -81,7 +82,8 @@ export class PostsService {
         id: id,
         title: title,
         content: content,
-        imagePath: image
+        imagePath: image,
+        creator: null
       };
     }
     this.http

02d9f83be9f408d1f04332ea58d959e9e1753baa
Elliot <raziel2244@pm.me>
2019-02-19 19:07:27 +0000

    130 - Separating Middleware

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,52 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const multer = require("multer");
 const checkAuth = require("../middleware/check-auth");
-
-const MIME_TYPE_MAP = {
-  "image/png": "png",
-  "image/jpeg": "jpg",
-  "image/jpg": "jpg"
-};
-
-const storage = multer.diskStorage({
-  destination: (_req, file, cb) => {
-    const isValid = MIME_TYPE_MAP[file.mimetype];
-    let error = new Error("Invalid mime type");
-    if (isValid) {
-      error = null;
-    }
-    cb(error, "server/images");
-  },
-  filename: (_req, file, cb) => {
-    const name = file.originalname
-      .toLowerCase()
-      .split(" ")
-      .join("-");
-    const ext = MIME_TYPE_MAP[file.mimetype];
-    cb(null, name + "-" + Date.now() + "." + ext);
-  }
-});
-
+const checkMime = require("../middleware/check-mime");
 const PostController = require("../controllers/posts");
 
-router.post(
-  "",
-  checkAuth,
-  multer({
-    storage: storage
-  }).single("image"),
-  PostController.postCreate
-);
+router.post("", checkAuth, checkMime, PostController.postCreate);
 
-router.put(
-  "/:id",
-  checkAuth,
-  multer({
-    storage: storage
-  }).single("image"),
-  PostController.postUpdate
-);
+router.put("/:id", checkAuth, checkMime, PostController.postUpdate);
 
 router.get("", PostController.postFetchAll);
 
